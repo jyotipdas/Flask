26b0538b1a3869dd6e4a8aa7583841e5370c2cab
jyotipdas <jpdas27@gmail.com>
2017-09-13 20:30:44 +0530

    09-13-1

--- app.py
+++ app.py
@@ -2,12 +2,46 @@ from flask import Flask, render_template,request
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'Thisismysecretkey'
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lf3yS8UAAAAAEDgFWIeKHal4Vem1HSjDy7br3rr'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lf3yS8UAAAAAExePlZihuoFhiZIcZOKWskui3sd'
 app.config['TESTING'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///leave.db"
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True,nullable=False)
+    password = db.Column(db.String(30),nullable=False)
+
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
+
+    def __repr__(self):
+        return '{}={}'.format(self.username,self.password)
+
+class LeaveDetail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usr_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    sdate = db.Column(db.DateTime,nullable=False)
+    edate = db.Column(db.DateTime,nullable=False)
+
+    usr_idR = db.relationship('users', foreign_keys='leavedetail.usr_id')
+
+    def __init__(self,usr_id,sdate,edate):
+        self.usr_id = usr_id
+        self.sdate = sdate
+        self.edate = edate
+
+    def __repr__(self):
+        return '{}={}'.format(self.sdate,self.edate)
+
 
 
 class LoginForm(FlaskForm):
