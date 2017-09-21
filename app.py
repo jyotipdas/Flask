aa5388261a25468781b517e84b473ca70bbeedd0
jyotipdas <jpdas27@gmail.com>
2017-09-21 15:20:39 +0530

    0921-01

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python2.7
-from flask import Flask, render_template,request, url_for, redirect, session, g
+from flask import Flask, render_template,request, url_for, redirect
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import  check_password_hash
 from flask_login import LoginManager, login_required, login_user,UserMixin, logout_user,current_user
-from datetime import datetime
 from sqlalchemy import text
 import atexit
 from apscheduler.scheduler import Scheduler
-#import logging
+import datetime
 
 
 app = Flask(__name__)
 cron = Scheduler(daemon=True)
 cron.start()
-#logging.basicConfig()
 
 app.config['SECRET_KEY'] = ';Y8m4e#PUP\qQR]+"`ZAM(&td{8utWN?CtHXg6X(-z!$XP4?(t)~g4Kk9xgr8}ZaH]eGx(:uvNE}GVp;'
-app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lf3yS8UAAAAAEDgFWIeKHal4Vem1HSjDy7br3rr'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LePzS8UAAAAADoA_QPfGVUArvWnA0oF9eZi7-L7'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6Lf3yS8UAAAAAExePlZihuoFhiZIcZOKWskui3sd'
 app.config['TESTING'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///leave.db"
@@ -66,7 +64,6 @@ def login():
         if db_usr:
             if check_password_hash(db_usr.password,form.password.data):
                 login_user(db_usr, remember=True)
-
                 return render_template('welcome.html', name=form.username.data)
             else:
                 return render_template('login.html',form=form, error='Password is not matching...')
@@ -84,12 +81,12 @@ def plan():
     if request.method == 'POST':
         if request.form['sdate'] > request.form['edate'] :
             return render_template('plan.html', error='Start date is greater than End date')
-        sdate = datetime.strptime(str(request.form['sdate']),'%Y-%m-%d')
-        edate = datetime.strptime(str(request.form['edate']),'%Y-%m-%d')
+        sdate = datetime.datetime.strptime(str(request.form['sdate']),'%Y-%m-%d')
+        edate = datetime.datetime.strptime(str(request.form['edate']),'%Y-%m-%d')
         days = int(request.form['days'])
         balance = Users.query.filter_by(id=current_user.get_id()).first()
         dbbalance= balance.balance
-        ctime=datetime.now()
+        ctime=datetime.datetime.now()
         if int(days) <= dbbalance :
             updated_balance = dbbalance - int(days)
             balance.balance = updated_balance
@@ -135,11 +132,13 @@ def logout():
 def page_not_found(e):
     return render_template('404.html',error=e)
 
-@cron.interval_schedule(hours=5, start_date='2017-09-20 18:00')
+@cron.cron_schedule(day='1')
 def job_function():
     for row in Users.query.all():
         row.balance = row.balance + 2
-    db.session.commit()
+        db.session.commit()
+
+
 atexit.register(lambda: cron.shutdown(wait=False))
 
 if __name__ == "__main__":
